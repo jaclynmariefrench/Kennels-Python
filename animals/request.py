16b68c7968e650a0d2f1baaafd3eeefa33a2fcb2
Jaclyn French <jaclynmariefrench@gmail.com>
2021-07-12 16:06:23 -0500

    fixed request_handler

--- animals/request.py
+++ animals/request.py
@@ -23,12 +23,6 @@ ANIMALS = [
 ]
 
 
-def get_all_animals():
-    """returning the animal dictonaries
-    """
-    return ANIMALS
-
-# Function with a single parameter
 def get_single_animal(id):
     """loop over animal dictonaries
     """
@@ -44,3 +38,30 @@ def get_single_animal(id):
             requested_animal = animal
 
     return requested_animal
+
+
+def get_all_animals():
+    """returning the animal dictonaries
+    """
+    return ANIMALS
+
+# Function with a single parameter
+
+
+def create_animal(animal):
+    """get id for animals
+    """
+    # Get the id value of the last animal in the list
+    max_id = ANIMALS[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the animal dictionary
+    animal["id"] = new_id
+
+    # Add the animal dictionary to the list
+    ANIMALS.append(animal)
+
+    # Return the dictionary with `id` property added
+    return animal
--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,8 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal
+from animals.request import get_all_animals, get_single_animal
+from customers import get_all_customers, get_single_customer
+from locations import get_all_locations, get_single_location
+from employees import get_all_employees, get_single_employee
 
 
 # Here's a class. It inherits from another class.
@@ -41,21 +44,41 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Here's a method on the class that overrides the parent's method.
     # It handles any GET request.
     def do_GET(self):
-        """Getting the single animal
+        """Getting the single customer
         """
         self._set_headers(200)
         response = {}  # Default response
 
         # Parse the URL and capture the tuple that is returned
         (resource, id) = self.parse_url(self.path)
+        # customers
+        if resource == "customers":
+            if id is not None:
+                response = f"{get_single_customer(id)}"
 
-        if resource == "animals":
+            else:
+                response = f"{get_all_customers()}"
+        # animals
+        elif resource == "animals":
             if id is not None:
                 response = f"{get_single_animal(id)}"
 
             else:
                 response = f"{get_all_animals()}"
+        # locations
+        elif resource == "locations":
+            if id is not None:
+                response = f"{get_single_location(id)}"
 
+            else:
+                response = f"{get_all_locations()}"
+        # employees
+        elif resource == "employees":
+            if id is not None:
+                response = f"{get_single_employee(id)}"
+
+            else:
+                response = f"{get_all_employees}"
         # This weird code sends a response back to the client
             self.wfile.write(response.encode())
 
@@ -85,8 +108,8 @@ class HandleRequests(BaseHTTPRequestHandler):
         """indexing the path
         """
         # Just like splitting a string in JavaScript. If the
-        # path is "/animals/1", the resulting list will
-        # have "" at index 0, "animals" at index 1, and "1"
+        # path is "/customers/1", the resulting list will
+        # have "" at index 0, "customers" at index 1, and "1"
         # at index 2.
         path_params = path.split("/")
         resource = path_params[1]
@@ -98,9 +121,9 @@ class HandleRequests(BaseHTTPRequestHandler):
             # This is the new parseInt()
             id = int(path_params[2])
         except IndexError:
-            pass  # No route parameter exists: /animals
+            pass  # No route parameter exists: /customers
         except ValueError:
-            pass  # Request had trailing slash: /animals/
+            pass  # Request had trailing slash: /customers/
 
         return (resource, id)  # This is a tuple
 
